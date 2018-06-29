4922ca5304bf3d5e14aecd5edca5d30b1a0f91f4
Hans Kirchner <hans-kirchner@gmx.de>
2018-06-29 22:01:59 +0200

    Add .clang-format and format code (make format)

--- satsub.cpp
+++ satsub.cpp
@@ -3,8 +3,8 @@
 #include <immintrin.h>
 
 void satsub_naive(std::uint8_t *data, std::size_t len, std::uint8_t y) {
-    for(std::size_t i=0; i<len; i++) {
-        if(data[i] >= y) {
+    for (std::size_t i = 0; i < len; i++) {
+        if (data[i] >= y) {
             data[i] -= y;
         } else {
             data[i] = 0;
@@ -13,7 +13,7 @@ void satsub_naive(std::uint8_t *data, std::size_t len, std::uint8_t y) {
 }
 
 void satsub_branchless(std::uint8_t *data, std::size_t len, std::uint8_t y) {
-    for(std::size_t i=0; i<len; i++) {
+    for (std::size_t i = 0; i < len; i++) {
         // https://stackoverflow.com/a/33482123
         std::uint8_t x = data[i];
         std::uint8_t res = x - y;
@@ -26,29 +26,29 @@ void satsub_sse(std::uint8_t *data, std::size_t len, std::uint8_t y) {
     // https://stackoverflow.com/a/1898156
     // https://stackoverflow.com/a/11298461
     std::size_t offset = 16 - (reinterpret_cast<std::uintptr_t>(data) % 16);
-    if(offset > 0) {
+    if (offset > 0) {
         satsub_branchless(data, offset, y);
     }
 
     std::size_t i = offset;
 
     const auto y_vec = _mm_set1_epi8(y);
-    const std::size_t cnt = (len-offset) / 16;
-    for(std::size_t j=0; j<cnt; j++) {
-        const auto x = _mm_load_si128(reinterpret_cast<__m128i*>(&data[i]));
+    const std::size_t cnt = (len - offset) / 16;
+    for (std::size_t j = 0; j < cnt; j++) {
+        const auto x = _mm_load_si128(reinterpret_cast<__m128i *>(&data[i]));
         const auto result = _mm_subs_epu8(x, y_vec);
-        _mm_store_si128(reinterpret_cast<__m128i*>(&data[i]), result);
+        _mm_store_si128(reinterpret_cast<__m128i *>(&data[i]), result);
         i += 16;
     }
 
-    satsub_branchless(&data[i], len-i, y);
+    satsub_branchless(&data[i], len - i, y);
 }
 
 void satsub_builtin(std::uint8_t *data, std::size_t len, std::uint8_t y) {
-    for(std::size_t i=0; i<len; i++) {
+    for (std::size_t i = 0; i < len; i++) {
         auto x = data[i];
-        bool no_overflow = ! __builtin_sub_overflow(x, y, &x);
-        x &= - no_overflow;
+        bool no_overflow = !__builtin_sub_overflow(x, y, &x);
+        x &= -no_overflow;
         data[i] = x;
     }
 }
--- test.cpp
+++ test.cpp
@@ -1,37 +1,43 @@
-#include <gtest/gtest.h>
-#include <cstdint>
 #include "satsub.hpp"
+#include <cstdint>
+#include <gtest/gtest.h>
 
 constexpr std::size_t SIZE = 18;
-constexpr std::uint8_t input[SIZE]{0,1,2,3,4,5,6,7,8,9,255,254,253,252,251,250,249,0};
-constexpr std::uint8_t result1[SIZE]{0,0,1,2,3,4,5,6,7,8,254,253,252,251,250,249,248,0};
-constexpr std::uint8_t result3[SIZE]{0,0,0,0,1,2,3,4,5,6,252,251,250,249,248,247,246,0};
+constexpr std::uint8_t input[SIZE]{0, 1,   2,   3,   4,   5,   6,   7,   8,
+                                   9, 255, 254, 253, 252, 251, 250, 249, 0};
+constexpr std::uint8_t result1[SIZE]{0, 0,   1,   2,   3,   4,   5,   6,   7,
+                                     8, 254, 253, 252, 251, 250, 249, 248, 0};
+constexpr std::uint8_t result3[SIZE]{0, 0,   0,   0,   1,   2,   3,   4,   5,
+                                     6, 252, 251, 250, 249, 248, 247, 246, 0};
 constexpr std::uint8_t result255[SIZE]{0};
 
 class SatSubTest : public ::testing::TestWithParam<std::uint8_t> {
 protected:
     alignas(16) std::uint8_t data[SIZE];
 
-    const std::uint8_t* getExpectedResult(std::uint8_t y) {
-        switch(y) {
-            case 0: return input;
-            case 1: return result1;
-            case 3: return result3;
-            case 255: return result255;
-            default: throw std::invalid_argument("Invalid test parameter");
+    const std::uint8_t *getExpectedResult(std::uint8_t y) {
+        switch (y) {
+        case 0:
+            return input;
+        case 1:
+            return result1;
+        case 3:
+            return result3;
+        case 255:
+            return result255;
+        default:
+            throw std::invalid_argument("Invalid test parameter");
         }
     }
 
-    virtual void SetUp() {
-        memcpy(data, input, SIZE);
-    }
+    virtual void SetUp() { memcpy(data, input, SIZE); }
 };
 
 TEST_P(SatSubTest, naive) {
     satsub_naive(data, SIZE, GetParam());
 
     const auto expectedResult = getExpectedResult(GetParam());
-    for(std::size_t i=0; i<SIZE; i++) {
+    for (std::size_t i = 0; i < SIZE; i++) {
         EXPECT_EQ(data[i], expectedResult[i]) << "Index " << i;
     }
 }
@@ -40,7 +46,7 @@ TEST_P(SatSubTest, branchless) {
     satsub_branchless(data, SIZE, GetParam());
 
     const auto expectedResult = getExpectedResult(GetParam());
-    for(std::size_t i=0; i<SIZE; i++) {
+    for (std::size_t i = 0; i < SIZE; i++) {
         EXPECT_EQ(data[i], expectedResult[i]) << "Index " << i;
     }
 }
@@ -49,7 +55,7 @@ TEST_P(SatSubTest, sse_aligned) {
     satsub_sse(data, SIZE, GetParam());
 
     const auto expectedResult = getExpectedResult(GetParam());
-    for(std::size_t i=0; i<SIZE; i++) {
+    for (std::size_t i = 0; i < SIZE; i++) {
         EXPECT_EQ(data[i], expectedResult[i]) << "Index " << i;
     }
 }
@@ -58,24 +64,25 @@ TEST_P(SatSubTest, builtin) {
     satsub_builtin(data, SIZE, GetParam());
 
     const auto expectedResult = getExpectedResult(GetParam());
-    for(std::size_t i=0; i<SIZE; i++) {
+    for (std::size_t i = 0; i < SIZE; i++) {
         EXPECT_EQ(data[i], expectedResult[i]) << "Index " << i;
     }
 }
 
-INSTANTIATE_TEST_CASE_P(SimpleValues, SatSubTest, ::testing::Values(0, 1, 3, 255));
+INSTANTIATE_TEST_CASE_P(SimpleValues, SatSubTest,
+                        ::testing::Values(0, 1, 3, 255));
 
 TEST(Satsub, SSE_BadAlignment) {
     std::uint8_t data[SIZE];
-    for(auto o=0; o<16; o++) {
+    for (auto o = 0; o < 16; o++) {
         memcpy(data, input, SIZE);
 
-        if(o>0) {
+        if (o > 0) {
             satsub_naive(data, o, 1);
         }
-        satsub_sse(&data[o], SIZE-o, 1);
+        satsub_sse(&data[o], SIZE - o, 1);
 
-        for(std::size_t i=0; i<SIZE; i++) {
+        for (std::size_t i = 0; i < SIZE; i++) {
             EXPECT_EQ(data[i], result1[i]) << "Offset " << o << " Index " << i;
         }
     }
